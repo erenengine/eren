76ca1aef423b4900f45def1555267ec16b324db4
erenengine <erenyeagerdeveloper@gmail.com>
2025-05-23 16:52:19 +0900

    feat: Simplify context initialization in Renderer::new method

--- eren_rs_2d/src/rendering/renderer.rs
+++ eren_rs_2d/src/rendering/renderer.rs
@@ -4,7 +4,7 @@ pub struct Renderer {}
 
 impl Renderer {
     pub fn new(width: u32, height: u32, title: &str) -> Self {
-        let context = Context::new(width, height, title, |path| {});
+        Context::new(width, height, title, |path| {});
         Self {}
     }
 }
